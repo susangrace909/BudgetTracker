39c822b8a620e768a22cf78f9973eaed02e1592c
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-09-15 22:31:29 -0400

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,71 +23,53 @@ const FILES_TO_CACHE = [
 // install service worker and add files to the precache, so that the application can use the cache
 self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-
-  self.skipWaiting();
 });
 
-//!!
-// Activate the service worker and remove old data from the cache
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
+// clear out any old data from the cache and, in the same step, tell the service worker how to manage caches
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add the current cache to the keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      // returns a promise that resolves once all old versions of the cache have been deleted
       return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
           }
         })
       );
     })
   );
-
-  self.clients.claim();
 });
 
-// Intercept fetch requests
-self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(evt.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
+// intercept fetch requests
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith();
+});
 
-              return response;
-            })
-            .catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        })
-        .catch((err) => console.log(err))
-    );
-
-    return;
+// determine if the resource already exists in caches
+caches.match(e.request).then(function (request) {
+  // If it does, log the URL to the console with a message and then return the cached resource
+  if (request) {
+    console.log("responding with cache : " + e.request.url);
+    return request;
   }
+});
 
-  evt.respondWith(
-    fetch(evt.request).catch(function () {
-      return caches.match(evt.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (evt.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
-      });
-    })
-  );
+// determine if the resource is not already exists in caches
+caches.match(e.request).then(function (request) {
+  // If it does not, allow the resource to be retrieved from the online network as usual
+  return request || fetch(e.request);
 });
