b70349d08bdeeeaf21d0980125aa034fbc9e398c
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-09-15 22:56:41 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb:", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
